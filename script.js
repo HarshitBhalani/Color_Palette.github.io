f35364bb186847cc96cc68fc35934864f05c43e2
Harshit Bhalani <125954651+HarshitBhalani@users.noreply.github.com>
2023-03-07 23:46:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const colorPalette = document.getElementById("color-palette");
-const colorBoxes = document.querySelectorAll(".color-box");
-const generateButton = document.getElementById("generate-btn");
+// JavaScript code to handle the color boxes
 
-function generateRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+let colorBoxes = document.querySelectorAll(".color-box");
+
+colorBoxes.forEach(function(box) {
+	box.addEventListener("click", function() {
+		let color = this.style.backgroundColor;
+		let hexColor = rgbToHex(color);
+		navigator.clipboard.writeText(hexColor);
+		alert("Color " + hexColor + " copied to clipboard!");
+	});
+});
+
+// Function to convert RGB color to HEX color
+
+function rgbToHex(rgbColor) {
+  let rgb = rgbColor.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  function hex(num) {
+    let hex = Number(num).toString(16);
+    if (hex.length < 2) {
+      hex = "0" + hex;
+    }
+    return hex;
   }
-  return color;
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
-
-function generatePalette() {
-  colorBoxes.forEach((box) => {
-    const color = generateRandomColor();
-    box.style.backgroundColor = color;
-    box.innerText = color;
-  });
-}
-
-generatePalette();
-
-generateButton.addEventListener("click", generatePalette);
-    
