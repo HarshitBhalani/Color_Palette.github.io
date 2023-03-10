ce59e12e70de5d73eab151d8a6bfefc6fafe98da
Harshit Bhalani <125954651+HarshitBhalani@users.noreply.github.com>
2023-03-10 17:07:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 let colorBoxes = document.querySelectorAll(".color-box");
 
-colorBoxes.forEach(function(box) {
-	box.addEventListener("click", function() {
-		let color = this.style.backgroundColor;
-		let hexColor = rgbToHex(color);
-		navigator.clipboard.writeText(hexColor);
-		alert("Color " + hexColor + " copied to clipboard!");
-	});
+colorBoxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    let color = this.style.backgroundColor;
+    let hexColor = rgbToHex(color);
+    navigator.clipboard.writeText(hexColor);
+    alert("Color " + hexColor + " copied to clipboard!");
+  });
 });
 
 // Function to convert RGB color to HEX color
@@ -24,3 +24,26 @@ function rgbToHex(rgbColor) {
   }
   return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
+
+
+// Get the button and the SVG icon
+var myButton = document.getElementById("myBtn");
+var myIcon = myButton.getElementsByTagName("svg")[0];
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
